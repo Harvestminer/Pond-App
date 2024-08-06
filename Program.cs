1dea780a708676b1200a22fe2aa6bb7fa359b40b
Harvestminer <harvestminer@gmail.com>
2024-08-06 17:51:08 -0500

    Fixes small compiler issue

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using PondWebApp;
-using PondWebApp.Data;
 using PondWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
